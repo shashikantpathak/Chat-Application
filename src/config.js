9aa77d6300f315e52d7dc5af619edd2c178e02f2
csuraz <shashikantpathakk@gmail.com>
2018-07-14 11:15:53 +0530

    initial changes

---
+++ src/config.js
@@ -0,0 +1,6 @@
+
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0ba36e3c-fc8e-41da-9c4c-afac74ed394f/token";
+
+const instanceLocator = "v1:us1:0ba36e3c-fc8e-41da-9c4c-afac74ed394f";
+
+export { tokenUrl, instanceLocator }
