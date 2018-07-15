68455889d04ef0feae6b66f22885a9958ef3b4cc
Shashikant Pathak <35792373+shashikantpathak@users.noreply.github.com>
2018-07-15 20:27:59 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // copy your tokenUrl from ChatKit app here
 const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/0ba36e3c-fc8e-41da-9c4c-afac74ed394f/token";
-
+// copy your instanceLocator from Chatkit app here
 const instanceLocator = "v1:us1:0ba36e3c-fc8e-41da-9c4c-afac74ed394f";
 
 export { tokenUrl, instanceLocator }
